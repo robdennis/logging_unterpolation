e1f85c1b4bd6c081578a19b658a98ec78d43ea03
Rob Dennis <rdennis@gmail.com>
2013-05-12 01:11:55 -0300

    added python 3.3 to classifiers
    [ci skip]

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
     'Programming Language :: Python :: 3.0',
     'Programming Language :: Python :: 3.1',
     'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
     'Operating System :: OS Independent',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Libraries :: Python Modules',
